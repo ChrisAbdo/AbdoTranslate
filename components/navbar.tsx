116f6337c1da9f7539464c5b324172711faa5c3b
ChrisAbdo <chris.j.abdo@gmail.com>
2023-03-12 19:37:50 -0400

    init

---
+++ components/navbar.tsx
@@ -0,0 +1,33 @@
+import { Github } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+
+export default function Navbar() {
+  return (
+    <header className="bg-white border-b border-black">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link href="/" className="-m-1.5 p-1.5 text-3xl">
+            AbdoTranslate
+          </Link>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+          <Button
+            variant="default"
+            onClick={() => {
+              window.open("https://www.github.com/chrisabdo", "_blank");
+            }}
+          >
+            <Github />
+            &nbsp;Star on GitHub
+          </Button>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,242 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../components/ui/select";
+import { ArrowLeftRight, Loader2, Volume2 } from "lucide-react";
+import { Button } from "../components/ui/button";
 
 const Home: NextPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [bio, setBio] = useState("");
+  const [vibe, setVibe] = useState("Spanish");
+  const [generatedBios, setGeneratedBios] = useState<String>("");
+
+  const bioRef = useRef<null | HTMLDivElement>(null);
+
+  const prompt = "Please translate " + bio + " to " + vibe;
+
+  const generateBio = async (e: any) => {
+    e.preventDefault();
+    setGeneratedBios("");
+    setLoading(true);
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      setGeneratedBios((prev) => prev + chunkValue);
+    }
+    setLoading(false);
+  };
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Twitter Bio Generator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <main>
+        {/* <button onClick={generateBio}>Generate Bio</button>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
+        {loading ? <p>Loading...</p> : <p>{generatedBios}</p>} */}
+        <div className="flex flex-col md:flex-row">
+          {/* LEFT PANEL */}
+          <div className="border-r border-black p-4 md:w-1/2">
+            <div className="flex justify-between">
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="English" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="English">English</SelectItem>
+                  <SelectItem value="Spanish">Spanish</SelectItem>
+                  <SelectItem value="French">French</SelectItem>
+                  <SelectItem value="German">German</SelectItem>
+                  <SelectItem value="Italian">Italian</SelectItem>
+                  <SelectItem value="Japanese">Japanese</SelectItem>
+                  <SelectItem value="Korean">Korean</SelectItem>
+                  <SelectItem value="Portuguese">Portuguese</SelectItem>
+                  <SelectItem value="Russian">Russian</SelectItem>
+                  <SelectItem value="Chinese">Chinese</SelectItem>
+                  <SelectItem value="Arabic">Arabic</SelectItem>
+                  <SelectItem value="Hindi">Hindi</SelectItem>
+                  <SelectItem value="Swedish">Swedish</SelectItem>
+                  <SelectItem value="Finnish">Finnish</SelectItem>
+                  <SelectItem value="Norwegian">Norwegian</SelectItem>
+                </SelectContent>
+              </Select>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+              <Button variant="ghost">
+                <ArrowLeftRight />
+              </Button>
+            </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+            <div className="relative mt-4">
+              <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+                <textarea
+                  rows={2}
+                  name="description"
+                  id="description"
+                  className="block w-full resize-none border-0 py-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="Translate any text to any language here"
+                  defaultValue={""}
+                  onChange={(e) => setBio(e.target.value)}
+                />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+                {/* Spacer element to match the height of the toolbar */}
+                <div aria-hidden="true">
+                  <div className="py-2">
+                    <div className="h-9" />
+                  </div>
+                  <div className="h-px" />
+                  <div className="py-2">
+                    <div className="py-px">
+                      <div className="h-9" />
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              <div className="absolute inset-x-px bottom-0">
+                {/* Actions: These are just examples to demonstrate the concept, replace/wire these up however makes sense for your project. */}
+                <div className="flex flex-nowrap justify-end space-x-2 py-2 px-2 sm:px-3"></div>
+                <div className="flex items-center justify-between space-x-3 border-t border-gray-200 px-2 py-2 sm:px-3">
+                  <div className="flex-shrink-0">
+                    {!loading && (
+                      <Button onClick={generateBio} variant="default">
+                        Translate
+                      </Button>
+                    )}
+                    {loading && (
+                      <Button variant="default">
+                        Translating <Loader2 className="animate-spin" />
+                      </Button>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* RIGHT PANEL */}
+          <div className="p-4 md:w-1/2">
+            <div>
+              <Select
+                defaultValue={vibe}
+                onValueChange={(value) => setVibe(value)}
+              >
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Spanish" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="Spanish">Spanish</SelectItem>
+                  <SelectItem value="English">English</SelectItem>
+                  <SelectItem value="French">French</SelectItem>
+                  <SelectItem value="German">German</SelectItem>
+                  <SelectItem value="Italian">Italian</SelectItem>
+                  <SelectItem value="Japanese">Japanese</SelectItem>
+                  <SelectItem value="Korean">Korean</SelectItem>
+                  <SelectItem value="Portuguese">Portuguese</SelectItem>
+                  <SelectItem value="Russian">Russian</SelectItem>
+                  <SelectItem value="Chinese">Chinese</SelectItem>
+                  <SelectItem value="Arabic">Arabic</SelectItem>
+                  <SelectItem value="Hindi">Hindi</SelectItem>
+                  <SelectItem value="Swedish">Swedish</SelectItem>
+                  <SelectItem value="Finnish">Finnish</SelectItem>
+                  <SelectItem value="Norwegian">Norwegian</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="relative mt-4">
+              <div className="overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+                <textarea
+                  rows={2}
+                  name="description"
+                  id="description"
+                  className="block w-full resize-none border-0 py-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="Your translated text will appear here"
+                  // @ts-ignore
+                  value={generatedBios}
+                  defaultValue={""}
+                  disabled
+                />
+
+                {/* Spacer element to match the height of the toolbar */}
+                <div aria-hidden="true">
+                  <div className="py-2">
+                    <div className="h-9" />
+                  </div>
+                  <div className="h-px" />
+                  <div className="py-2">
+                    <div className="py-px">
+                      <div className="h-9" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="absolute inset-x-px bottom-0">
+                  {/* Actions: These are just examples to demonstrate the concept, replace/wire these up however makes sense for your project. */}
+                  <div className="flex flex-nowrap justify-end space-x-2 py-2 px-2 sm:px-3"></div>
+                  {generatedBios && (
+                    <div className="flex items-center justify-between space-x-3 border-t border-gray-200 px-2 py-2 sm:px-3">
+                      <motion.div
+                        className="flex-shrink-0 space-x-2"
+                        initial={{ x: -20, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 0.2 }}
+                      >
+                        <Button
+                          onClick={() => {
+                            // @ts-ignore
+                            navigator.clipboard.writeText(generatedBios);
+                          }}
+                          variant="outline"
+                        >
+                          Copy
+                        </Button>
+                        <Button variant="outline">Share</Button>
+                      </motion.div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
